11d4406689f900adfaa69d1ee945ba2212585cc8
YoungBeenKim <brokentime906@gmail.com>
2019-07-06 17:23:56 +0900

    Post에 대한 admin을 커스텀

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post  # 여기가 추가된줄
 # Register your models here.
+
+#admin.site.register(Post) #admin에다가 Post모델을 등록한다 # 여기가 추가된줄
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','created_at']
+    list_filter = ['updated_at']
+    search_fields = ['title']
+
+
